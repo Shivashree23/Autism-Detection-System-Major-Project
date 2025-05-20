01846e0e7ef7318d8e7dd37b805b176f736506ab
Shivashree23 <shivashreebhandari23@gmail.com>
2025-05-20 22:14:57 +0530

    updated navbar

--- src/Components/Header/NavBar.tsx
+++ src/Components/Header/NavBar.tsx
@@ -1,11 +1,11 @@
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/20/solid';
 import { SelectedPage } from '@/Components/Shared/Types';
 import { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 import useMediaQuery from '@/Hooks/useMediaQuery';
-
 import Links from './Links';
 import Button from '../UI/Button';
+
 type Props = {
   flexBetween: string;
   selectedPage: SelectedPage;
@@ -18,7 +18,7 @@ const NavBar = ({ flexBetween, selectedPage, setSelectedPage }: Props) => {
 
   return (
     <nav>
-      {isAboveMediumScreens && (
+      {isAboveMediumScreens ? (
         <div className={`${flexBetween} lg:gap-28 gap-20`}>
           <div className={`${flexBetween} gap-16`}>
             <Links
@@ -26,32 +26,37 @@ const NavBar = ({ flexBetween, selectedPage, setSelectedPage }: Props) => {
               setSelectedPage={setSelectedPage}
             />
           </div>
-          <Button>Log In</Button>
+          <Link to="/login">
+            <Button>Log In</Button>
+          </Link>
         </div>
-      )}
-      {!isAboveMediumScreens && (
-        <button onClick={() => setIsMenuToggled((prev) => !prev)}>
-          <Bars3Icon className="h-8 w-8" />
-        </button>
-      )}
-      {/* MOBILE MENU MODAL */}
-      {!isAboveMediumScreens && isMenuToggled && (
-        <div className="fixed right-0 top-0 z-40 h-80 rounded-es-3xl w-[175px] md:w-[300px] bg-secondary drop-shadow-2xl">
-          {/* CLOSE ICON */}
-          <div className="flex justify-end p-5 md:pr-16 sm:pt-10">
-            <button onClick={() => setIsMenuToggled((prev) => !prev)}>
-              <XMarkIcon className="h-10 w-10" />
-            </button>
-          </div>
+      ) : (
+        <>
+          <button onClick={() => setIsMenuToggled((prev) => !prev)}>
+            <Bars3Icon className="h-8 w-8" />
+          </button>
 
-          {/* MENU ITEMS */}
-          <div className=" ml-[20%] flex flex-col items-start gap-5 text-2xl">
-            <Links
-              selectedPage={selectedPage}
-              setSelectedPage={setSelectedPage}
-            />
-          </div>
-        </div>
+          {/* MOBILE MENU MODAL */}
+          {isMenuToggled && (
+            <div className="fixed right-0 top-0 z-40 h-80 rounded-es-3xl w-[175px] md:w-[300px] bg-secondary drop-shadow-2xl">
+              <div className="flex justify-end p-5 md:pr-16 sm:pt-10">
+                <button onClick={() => setIsMenuToggled(false)}>
+                  <XMarkIcon className="h-10 w-10" />
+                </button>
+              </div>
+
+              <div className="ml-[20%] flex flex-col items-start gap-5 text-2xl">
+                <Links
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link to="/login">
+                  <Button>Log In</Button>
+                </Link>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </nav>
   );
