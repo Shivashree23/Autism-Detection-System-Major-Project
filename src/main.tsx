22506af3520e52637df39cae51c463d287594763
Shivashree23 <shivashreebhandari23@gmail.com>
2025-05-20 22:09:14 +0530

    updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import AutismQuestionsPage from './AutismQuestionsPage';
+import LoginPage from './LoginPage';
+import { AuthProvider } from './contexts/AuthContext';
+import RequireAuth from './Components/RequireAuth'; // ✅ Adjust path if your folder is capitalized
 import './index.css';
-import Home from "./Components/Main/Home/Home";
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          {/* Public route */}
+          <Route path="/" element={<App />} />
+
+          {/* Protected route */}
+          <Route
+            path="/autism-test"
+            element={
+              <RequireAuth>
+                <AutismQuestionsPage />
+              </RequireAuth>
+            }
+          />
+
+          {/* Public login route */}
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
